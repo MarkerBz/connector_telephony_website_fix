ba8689543ee2605abb92c4823d06563dd94669d0
Marker.bz <manyatovskiy@gmail.com>
2017-04-26 20:33:22 +0300

    Новый модуль

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fixes Website errors when installed 'connector-telephony' modules",
+    'version': '10.0.1.0.0',
+    'author': 'Contractor Andrey S. 2017',
+    'description': """
+Fixes Website errors when installed 'connector-telephony' modules.
+    """,
+    'depends': [
+        'website_form',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
